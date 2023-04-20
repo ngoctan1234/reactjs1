e050bec16153c7b1de7ad70ae4744ad61f53b866
Duong Hoang Thanh <thanh1807@yahoo.com>
2023-04-20 17:21:30 +0700

    update lan 1

--- src/App.js
+++ src/App.js
@@ -1,23 +1,62 @@
-import logo from './logo.svg';
-import './App.css';
-
+import logo from "./logo.svg";
+import "./App.css";
+import StudentList from "./StudentList";
+import { useState, useEffect } from "react";
 function App() {
+  const [x, setX] = useState(0);
+  const [check, setCheck] = useState(true);
+  const [text, setText] = useState("Mèo");
+  const [name, setName] = useState("");
+  const [list, setList] = useState([1, 2, 3, 4]);
+  const [student, setStudent] = useState({
+    name: "Nguyễn Hoàng Gia Bảo",
+    old: 5,
+  });
+  const handle_Increase = () => {
+    setX((pre) => pre + 1);
+    setX((pre) => pre + 1);
+  };
+  const handle_Change_Text = (e) => {
+    e.preventDefault();
+    setText(name);
+    setName("");
+    setStudent((pre) => ({ ...pre, name: name }));
+  };
+  const handle_change_name = (event) => {
+    setName(event.target.value);
+    setStudent((pre) => ({ ...pre, name: name }));
+  };
+  console.log("re render");
+  const handle_toggle_student = () => {
+    setCheck((pre) => !pre);
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h2>React js</h2>
+      <h1>{x}</h1>
+      <h1>name:{text}</h1>
+      <h1>
+        Họ và tên: {student["name"]}, Tuổi: {student.old}{" "}
+        {JSON.stringify(student)}
+      </h1>
+      <button onClick={handle_Increase}>Increase</button>
+      <button onClick={handle_Change_Text}>Change Text</button>
+      <form onSubmit={handle_Change_Text}>
+        <input
+          type="text"
+          placeholder="Name"
+          value={name}
+          onChange={handle_change_name}
+        />
+      </form>
+
+      <button onClick={handle_toggle_student}>Toggle Student</button>
+      {check ? <StudentList /> : ""}
+      <ul>
+        {list.map((st, key) => {
+          return <li>{st}</li>;
+        })}
+      </ul>
     </div>
   );
 }
---
+++ src/StudentList.js
@@ -0,0 +1,4 @@
+function StudentList() {
+  return <h2>Danh sách sinh viên</h2>;
+}
+export default StudentList;
